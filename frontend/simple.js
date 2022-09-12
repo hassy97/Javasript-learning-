98c43ea46d1d9793cbb67571f92ad46edf08b6df
Hassy <97879833+hassy97@users.noreply.github.com>
2022-09-12 16:50:12 +0500

    practice stuff

---
+++ frontend/simple.js
@@ -0,0 +1,14 @@
+
+
+// ===
+// document.getElementsByClassName(".btn").addEventListener("click", function() {
+//     alert("Hello World!");
+//   });
+
+// document.getElementsByClassName("btn").innerHTML = "new_text";
+
+// document.getElementsByClassName("btn").
+
+function ChangeText(){
+    document.getElementById("123").innerHTML = "Hello";
+}
