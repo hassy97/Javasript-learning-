fd976ecad3483690a0fe9b16e303f2a8d907c2ec
Hassy <97879833+hassy97@users.noreply.github.com>
2022-09-21 17:25:55 +0500

    Add files via upload

---
+++ twitter login page/model.js
@@ -0,0 +1,8 @@
+// model to open and close a registration form 
+function openPopup(){
+    document.getElementById("myModal").style.display = "block";
+  }
+  function popupClose(){
+    document.getElementById("myModal").style.display="none";
+  }
+  
